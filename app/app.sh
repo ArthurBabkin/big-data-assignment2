b35762af5a741aac978825eba76238fe56eb3712
Arthur Babkin <artur.babkin.2015@gmail.com>
2026-04-04 23:36:53 +0300

    finalize pipeline and requirements

--- app/app.sh
+++ app/app.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-# Start ssh server
-service ssh restart 
 
-# Starting the services
+service ssh restart
+
 bash start-services.sh
 
-# Creating a virtual environment
 python3 -m venv .venv
 source .venv/bin/activate
 
-# Install any packages
-pip install -r requirements.txt  
+pip install -r requirements.txt
 
-# Package the virtual env.
 venv-pack -o .venv.tar.gz
 
-# Collect data
 bash prepare_data.sh
 
-
-# Run the indexer
 bash index.sh
 
-# Run the ranker
-bash search.sh "this is a query!"
+bash search.sh "history of music"
+bash search.sh "war and military conflict"
+bash search.sh "science technology innovation"
